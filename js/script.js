382e4063635d0c05d386db7ab7269a2d2b76142c
maxim <maxim@MacBook-Air-maxim.local>
2025-07-27 23:26:27 +0300

    init commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Здесь вы можете добавить JavaScript для интерактивных элементов,
+// например, для плавной прокрутки к секциям при клике на ссылки в навигации.
+
+// Пример плавной прокрутки (раскомментируйте, если нужно):
+/*
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+*/
